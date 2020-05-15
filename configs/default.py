6e676e3bf4747fd3dd05fae322b26c09750b3105
JiayouLu <jiayou.lu@plus.ai>
2020-05-15 11:38:57 +0800

    set recurrent=true & add entropy term

--- configs/default.py
+++ configs/default.py
@@ -21,25 +21,27 @@ default_config = dict(
         num_extra_rl_steps_posterior=400, # number of additional transitions to collect per task with z ~ posterior that are only used to train the policy and NOT the encoder
         num_train_steps_per_itr=2000, # number of meta-gradient steps taken per iteration
         num_evals=2, # number of independent evals
-        num_steps_per_eval=600,  # nuumber of transitions to eval on
+        num_steps_per_eval=600,  # number of transitions to eval on
         batch_size=256, # number of transitions in the RL batch
         embedding_batch_size=64, # number of transitions in the context batch
         embedding_mini_batch_size=64, # number of context transitions to backprop through (should equal the arg above except in the recurrent encoder case)
         max_path_length=200, # max path length for this environment
         discount=0.99, # RL discount factor
         soft_target_tau=0.005, # for SAC target network update
+        alpha=0.05,
+        automatic_entropy_tuning=True,
         policy_lr=3E-4,
         qf_lr=3E-4,
         vf_lr=3E-4,
+        lr=3E-4,
         context_lr=3e-4,
         reward_scale=5., # scale rewards before constructing Bellman update, effectively controls weight on the entropy of the policy
         sparse_rewards=False, # whether to sparsify rewards as determined in env
         kl_lambda=.1, # weight on KL divergence term in encoder loss
-        use_information_bottleneck=True, # False makes latent context deterministic
-        use_next_obs_in_context=False, # use next obs if it is useful in distinguishing tasks
+        use_information_bottleneck=False, # False makes latent context deterministic
         update_post_train=1, # how often to resample the context when collecting data during training (in trajectories)
         num_exp_traj_eval=1, # how many exploration trajs to collect before beginning posterior sampling at test time
-        recurrent=False, # recurrent or permutation-invariant encoder
+        recurrent=True, # recurrent or permutation-invariant encoder
         dump_eval_paths=False, # whether to save evaluation trajectories
     ),
     util_params=dict(
@@ -50,6 +52,3 @@ default_config = dict(
         docker=False, # TODO docker is not yet supported
     )
 )
-
-
-
