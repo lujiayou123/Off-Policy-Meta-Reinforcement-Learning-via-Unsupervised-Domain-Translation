e6373c9ff644c7d462dbdfc8db2d08b5d0064bd8
JiayouLu <jiayou.lu@plus.ai>
2020-05-15 16:28:48 +0800

    use new RNN model,worked after first modification

--- rlkit/torch/networks.py
+++ rlkit/torch/networks.py
@@ -184,12 +184,14 @@ class RecurrentEncoder(FlattenMlp):
 
 # Recurrent neural network (many-to-one)
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.fc = nn.Linear(hidden_size, output_size)
+        self.output_size = output_size
+        self.register_buffer('hidden', torch.zeros(1, 1, self.hidden_size))
     
     def forward(self, x):
         # Set initial hidden and cell states 
@@ -197,9 +199,13 @@ class RNN(nn.Module):
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(ptu.device)
         
         # Forward propagate LSTM
-        out, _ = self.lstm(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
-        
+        # out, _ = self.lstm(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        out, (hn, cn) = self.lstm(x, (h0, c0))
+        self.hidden = hn
         # Decode the hidden state of the last time step
         out = self.fc(out[:, -1, :])
         return out
+
+    def reset(self, num_tasks=1):
+        self.hidden = self.hidden.new_full((1, num_tasks, self.hidden_size), 0)
 
